37971cad20712fc0b5bb4485dcc5da03cbfea9b5
Voorellen <47812588+Voorellen@users.noreply.github.com>
2019-03-05 16:46:39 +0100

    Initial datagrabber

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -5,12 +5,22 @@ import sys
 import nipype
 import nipype.pipeline as pe
 
+import nipype.interfaces.io as io
+import nipype.interfaces.utility as utility
 
+#Generic datagrabber module that wraps around glob in an
+io_DataGrabber = pe.Node(interface = io.DataGrabber(), name='io_DataGrabber')
+io_DataGrabber.inputs.template = dict(func='%d/func/%d_s1_r1.nii')
+io_DataGrabber.inputs.base_directory = '/project/3018028.06/LEX_ELLEN/data/'
+io_DataGrabber.inputs.template_args =  dict(func=[['subj_id', 'subj_id']])
 
+#Basic interface class generates identity mappings
+utility_IdentityInterface = pe.Node(utility.IdentityInterface(fields=["subject", "session", "run_no"]), name='utility_IdentityInterface', iterfield = ['subject'])
+utility_IdentityInterface.iterables = [('subject', [50, 99])]
 
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
-
+analysisflow.connect(utility_IdentityInterface, "subject", io_DataGrabber, "subj_id")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
